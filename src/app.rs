408723b281f22ac4dfd049599fac0a47dc2f4eca
Takahiro Itazuri <zulinx86@gmail.com>
2022-02-28 01:49:37 +0900

    Update error handling
    - Define a custom error (`BucketListError`).
    - Print error messages with `std::fmt::Display` in `main()`.
    - Refactor core features by using the custom error.

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
-use std::error::Error;
+use thiserror::Error;
 use indexmap::IndexMap;
 use serde::{Serialize, Deserialize};
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
 use std::io::BufReader;
 use std::time::{SystemTime, UNIX_EPOCH};
 
@@ -11,6 +11,18 @@ const DECAY: f32 = 0.925;
 const SEC_OF_DECAY: u64 = 86400;
 const ACTIVE_THRESHOLD: f32 = 0.1;
 
+#[derive(Debug, Error)]
+pub enum BucketListError {
+    #[error("No such an item (name: `{0}`).")]
+    NotFoundError(String),
+    #[error("I/O related error happened.")]
+    IoError(#[from] std::io::Error),
+    #[error("Time related error happened.")]
+    TimeError(#[from] std::time::SystemTimeError),
+    #[error("Serde related error happened.")]
+    SeredeError(#[from] serde_json::Error),
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Info {
     prio: f32,
@@ -19,29 +31,24 @@ pub struct Info {
     note: String,
 }
 
-pub fn read_file() -> Result<IndexMap<String, Info>, Box<dyn Error>> {
-    // create "~/.bucketlist" directory if not exist.
+pub fn read_file() -> Result<IndexMap<String, Info>, BucketListError> {
     let dir = match dirs::home_dir() {
         None => format!("./{}", CONFIG_DIRNAME),
-        Some(home) => {
-            let dir = format!("{}/{}", home.to_str().unwrap(), CONFIG_DIRNAME);
-            std::fs::create_dir_all(&dir)?;
-            dir
-        },
+        Some(home) => format!("{}/{}", home.to_str().unwrap(), CONFIG_DIRNAME),
     };
+    std::fs::create_dir_all(&dir)?;
 
-    // create "data.json" file if not exists.
-    let file = OpenOptions::new()
-        .write(true)
-        .read(true)
-        .create(true)
-        .open(format!("{}/{}", dir, DATA_FILENAME))?;
-    let reader = BufReader::new(file);
+    match File::open(format!("{}/{}", dir, DATA_FILENAME)) {
+        Err(_) => Ok(IndexMap::new()),
+        Ok(file) => {
+            let reader = BufReader::new(file);
 
-    match serde_json::from_reader(reader) {
-        // when succeeds, returns deserialized items.
-        Ok::<IndexMap<String, Info>, _>(mut items) => {
-            let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+            let mut items: IndexMap<String, Info> = serde_json::from_reader(reader)?;
+
+            let now = SystemTime::now()
+                .duration_since(UNIX_EPOCH)?
+                .as_secs();
+
             for (_, info) in items.iter_mut() {
                 let days = (now - info.last) / SEC_OF_DECAY;
                 info.prio *= DECAY.powi(days as i32);
@@ -50,13 +57,10 @@ pub fn read_file() -> Result<IndexMap<String, Info>, Box<dyn Error>> {
 
             Ok(items)
         },
-
-        // when fails, returns empty HashMap.
-        Err(_) => Ok(IndexMap::new())
     }
 }
 
-pub fn save_file(items: IndexMap<String, Info>) -> Result<(), Box<dyn Error>> {
+pub fn save_file(items: IndexMap<String, Info>) -> Result<(), BucketListError> {
     let dir = match dirs::home_dir() {
         None => format!("./{}", CONFIG_DIRNAME),
         Some(home) => format!("{}/{}", home.to_str().unwrap(), CONFIG_DIRNAME),
@@ -64,15 +68,20 @@ pub fn save_file(items: IndexMap<String, Info>) -> Result<(), Box<dyn Error>> {
 
     let file = OpenOptions::new()
         .write(true)
+        .create(true)
         .truncate(true)
         .open(format!("{}/{}", dir, DATA_FILENAME))?;
 
     serde_json::to_writer(&file, &items)?;
+
     Ok(())
 }
 
-pub fn add_or_incr(mut items: IndexMap<String, Info>, name: String) -> Result<IndexMap<String, Info>, Box<dyn Error>> {
-    let now = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs();
+pub fn add_or_incr(mut items: IndexMap<String, Info>, name: String) -> Result<IndexMap<String, Info>, BucketListError> {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)?
+        .as_secs();
+
     match items.get_mut(&name) {
         Some(info) => {
             info.prio += 1.0;
@@ -93,32 +102,30 @@ pub fn add_or_incr(mut items: IndexMap<String, Info>, name: String) -> Result<In
     Ok(items)
 }
 
-pub fn note(mut items: IndexMap<String, Info>, name: String, note: String) -> Result<IndexMap<String, Info>, Box<dyn Error>> {
+pub fn note(mut items: IndexMap<String, Info>, name: String, note: String) -> Result<IndexMap<String, Info>, BucketListError> {
     match items.get_mut(&name) {
+        None => Err(BucketListError::NotFoundError(name)),
         Some(info) => {
             info.note = note;
             println!("The note of `{}` is upated.", name);
+            Ok(items)
         },
-        None => println!("No such an item (name: `{}`)", name)
     }
-    Ok(items)
 }
 
-pub fn del(mut items: IndexMap<String, Info>, name: String) -> Result<IndexMap<String, Info>, Box<dyn Error>> {
+pub fn del(mut items: IndexMap<String, Info>, name: String) -> Result<IndexMap<String, Info>, BucketListError> {
     match items.remove(&name) {
+        None => Err(BucketListError::NotFoundError(name)),
         Some(info) => {
             println!("`{}` is deleted.", name);
             log::info!("{:#?}", info);
+            Ok(items)
         },
-        None => println!("No such an item (name: `{}`)", name),
-    };
-    Ok(items)
+    }
 }
 
-pub fn ls(mut items: IndexMap<String, Info>, all: bool) -> Result<IndexMap<String, Info>, Box<dyn Error>> {
-    items.sort_by(
-        |_, v1, _, v2| v1.prio.partial_cmp(&v2.prio).unwrap().reverse()
-    );
+pub fn ls(mut items: IndexMap<String, Info>, all: bool) -> Result<IndexMap<String, Info>, BucketListError> {
+    items.sort_by(|_, v1, _, v2| v1.prio.partial_cmp(&v2.prio).unwrap().reverse());
 
     for (k, v) in &items {
         if all == false && v.prio <= ACTIVE_THRESHOLD {
@@ -126,5 +133,6 @@ pub fn ls(mut items: IndexMap<String, Info>, all: bool) -> Result<IndexMap<Strin
         }
         println!("{}: {:#?}", k, v);
     }
+
     Ok(items)
 }
